577b5476a38b0c7671518ddece908cf82d350b6f
d1Mkazi <dmitriivkyt@gmail.com>
2023-09-26 16:04:16 +0600

    Added ricochets

--- Scripts/shellDB.lua
+++ Scripts/shellDB.lua
@@ -1,17 +1,39 @@
 dofile("utils.lua")
 
+---@param result RaycastResult
+local function getAngle(result)
+    local angle = math.deg(math.acos(result.directionWorld:dot(result.normalWorld) / result.directionWorld:length() * result.normalLocal:length()))
+    while angle > 90 do angle = angle - 90 end
+    return math.floor(angle + 0.5)
+end
+
+---@param base number base durability of the block/shape
+---@param angle number hit angle
+local function getDurability(base, angle)
+    -- idk how to call them so...
+    local x = math.floor(angle * 100 / 90)
+    local y = (100 - x) * 0.85
+    local z = base / 100
+    return base + z * y
+end
+
 ShellDB = {
-    --[[     -----------------------------------DEPRECATED AND I AM TOO LAZY TO CHANGE THAT--------------------------------------------------------------------------
+    --[[
         template:
         Shell = {
             type (string)
-            UUID (string) -- uuid of bullet to render
+            shellUUID (string) -- uuid of the shell
+            bulletUUID (string) -- uuid of the bullet
             initialSpeed = (number)
-            friction = (number) -- percentage of speed reduction per block (0.001 = 1%)
-            penetrationCapacity (number) -- the maximum sum of blocks durability shell can break
+            friction = (number) -- percentage of speed reduction per frame / 1000
+            penetrationCapacity (number) -- the maximum sum of block durabilities shell can break
+            penetrationLoss (number)
             maxDurability (number) -- the maximum durability of a block shell can break
-            fuseSensitivity (number) -- the minimum sum of blocks durability shell must break to explode
+            fuseSensitivity (number) -- the minimum sum of block durabilities shell must break to explode
+            maxAngle (number) -- the angle the projectile ricochets at
             onHit (function(shelldata))
+
+            -- you may not set "penetrationCapacity", "penetrationLoss", "maxDurability", "fuseSensitivity" if they are not used
         }
     ]]
     AP = {
@@ -20,29 +42,31 @@ ShellDB = {
         bulletUUID = "ec19cdbf-865e-401c-9c5e-f122bed25800",
         initialSpeed = 240,
         friction = 0.003,
-        penetrationCapacity = 12,
-        maxDurability = 7,
+        penetrationCapacity = 20,
+        penetrationLoss = 2.5,
+        maxDurability = 7.6,
         fuseSensitivity = 5,
+        maxAngle = 18,
         onHit = function(data)
             local vel = data.vel
-            print("shell velocity:", vel)
             local dir = vel:normalize() / 4
-            print("shell direction:", dir)
 
-            local r = data.penetrationCapacity / 100 * 15
-            data.penetrationCapacity = math.random(data.penetrationCapacity - r, data.penetrationCapacity + r)
+            local capacity = data.penetrationCapacity
+            local random = capacity * 0.15
+            data.penetrationCapacity = capacity - math.random(random, -random)
+
             local toughness = 0
             local raycast = sm.physics.raycast
-            local dt = data.dt
             local hit, result = true, data.hit
-            print("\nnormalLocal =", result.normalLocal, "\nnormalWorld =", result.normalWorld)
+
+            local angle = getAngle(result)
+
             local point = nil
             while hit do
                 point = result.pointWorld
 
                 if result.type == "character" then
-                    print("char")
-                    sm.event.sendToPlayer(result:getCharacter():getPlayer(), "sv_e_receiveDamage", {damage = 100})
+                    sm.event.sendToPlayer(result:getCharacter():getPlayer(), "sv_e_receiveDamage", { damage = 100 }) -- why don't you work?
 
                 elseif result.type == "terrainSurface" or result.type == "terrainAsset" then
                     sm.physics.explode(point, 7, 1, 2, 70, "PropaneTank - ExplosionSmall")
@@ -51,6 +75,12 @@ ShellDB = {
                 elseif result.type == "body" then
                     local shape = result:getShape()
                     local durability = sm.item.getQualityLevel(shape.uuid)
+                    if angle <= data.maxAngle then
+                        data.vel = -vel:rotate(math.rad(180), result.normalWorld)
+                        data.penetrationCapacity = data.penetrationCapacity - durability * 0.2
+                        return true
+                    end
+                    durability = getDurability(durability, angle)
 
                     if durability > data.maxDurability or durability > data.penetrationCapacity then
                         sm.physics.explode(point, 1, 1, 2, 70, "PropaneTank - ExplosionSmall")
@@ -89,7 +119,6 @@ ShellDB = {
             end
 
             local pos = point + dir
-            print("Difference:", pos - point)
             shrapnelExplosion(pos, vel, 3, 120, 35)
             if data.fuseSensitivity > toughness then return true end
             shrapnelExplosion(pos, vel, 10, 120, 85)
