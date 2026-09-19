8d624bbb81162b1aa3385f36847d10cabb5cb2c6
d1Mkazi <dmitriivkyt@gmail.com>
2026-09-19 13:43:45 +0600

    Updated fetch function

--- Scripts/TankParts.lua
+++ Scripts/TankParts.lua
@@ -10,8 +10,23 @@ sm.TankParts = {
 
     -- fetch Breech and Shell data from JSON files
     fetch = function()
+        local REQUIRED_SHELL_FIELDS = {
+            type = "string",
+            initialSpeed = "number",
+            mass = "number",
+            caliber = "number",
+            headCurve = "number",
+            density = "number"
+        }
+
         local shapedb = sm.json.open("$CONTENT_88ba8635-775e-4759-9a69-3df71f653f19/Objects/Database/shapesets.shapedb")
-        for _, shapeset in pairs(shapedb.shapeSetList) do
+        local shapeSetList = shapedb.shapeSetList
+        for i = 1, #shapeSetList - 2 do
+            print("shapeSetList =", shapeSetList)
+            print("i =", i)
+            local shapeset = shapeSetList[i]
+            print("shapeset =", shapeset)
+
             if shapeset:sub(-9, -6) ~= "misc" then
                 print("OPEN [", shapeset, "]")
                 local breechset = sm.json.open(shapeset)
@@ -20,13 +35,29 @@ sm.TankParts = {
                 for _, part in pairs(breechset.partList) do
                     if part.type == "breech" then
                         breeches[#breeches + 1] = part.uuid
-                        print(("Added breech \"%s\""):format(part.name))
+                        print("Added breech \""..part.name.."\"")
                     elseif part.type == "shell" then
-                        print("Shell", part.name, "with breeches", breeches)
-                        ammodb[tostring(part.uuid)] = {
-                            breechlist = breeches,
-                            data = part.scripted.data.shellData
-                        }
+                        local shellData = part.scripted.data.shellData
+
+                        if type(shellData) ~= "table" then
+                            sm.log.error("Field \"shellData\" is missing or incorrect type in shell "..part.name)
+                        else
+                            local correct = true
+                            for key, ftype in pairs(REQUIRED_SHELL_FIELDS) do
+                                if type(shellData[key]) ~= ftype then
+                                    sm.log.error("Necessary field \""..key.."\" is missing or incorrect type in shell "..part.name)
+                                    correct = false
+                                end
+                            end
+
+                            if correct then
+                                ammodb[tostring(part.uuid)] = {
+                                    breechlist = breeches,
+                                    data = shellData
+                                }
+                                print("Added shell \""..part.name.."\"")
+                            end
+                        end
                     end
                 end
             end
